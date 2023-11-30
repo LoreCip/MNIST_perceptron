efd4365349c7751aeb899a4073c5f689f9456d55
Lorenzo Cipriani <lorenzo@DESKTOP-L6EDCMR>
2023-11-30 11:59:35 +0100

    Rewritten to add numba aceleration

--- MNIST.py
+++ MNIST.py
@@ -1,60 +1,68 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from numba import int64, float64
+
 from Perceptron import Perceptron
 
 def get_mnist():
     with np.load(f"train/mnist.npz") as f:
         images, labels = f["x_train"], f["y_train"]
-    images = images.astype("float32") / 255
+    images = images.astype("float64") / 255
     images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2]))
     labels = np.eye(10)[labels]
+    
     return images, labels
 
 def convert_to_input(img):
     from PIL import Image
     im1 = Image.open(img)
-    return ( 255 - np.array(im1.getdata(0)) )/ 255
+    return ( 255 - np.array(im1.getdata(0), order = 'C') ) / 255
 
 
 images, labels = get_mnist()
-
-
-## Define Perceptron
-parallel = True
-
-HiddenNodes = [784, 40, 10]
-N = Perceptron(HiddenNodes)
+images = np.ascontiguousarray(images)
+labels = np.ascontiguousarray(labels)
 
 training_inputs = images[:59500]
 training_output = labels[:59500]
-training_iterations = 4
 
 test_inputs = images[59500:]
 test_output = labels[59500:]
 
+
+## Define Perceptron
+
+HiddenNodes = np.array([784, 40, 10], dtype=np.int64, order = 'C')
+N = Perceptron(HiddenNodes, 3)
+
 ## Train
 
-N.set_train(training_inputs, training_output, training_iterations)
+N.train(training_inputs, training_output)
 
-## Test 1
+for it in range(N.training_iterations):
+    print(f'In the epoch number {it+1}, {round(N.guessed[it] * 100 / len(training_inputs), 2)}% of the training inputs were guessed right.')
+
+
+# Test 1
 
 img_from_file    = convert_to_input('test/1.png')
-output_from_file =1
+output_from_file = 1
 
-output = N.guess(img_from_file)
+output = N.guess(img_from_file[:, np.newaxis])
+
 plt.imshow(img_from_file.reshape(28, 28), cmap="Greys")
-plt.title(f"Is it a {output.argmax()}? It should be a {output_from_file}.");
+plt.title(f"Is it a {output.argmax()}? It should be a {output_from_file}.")
 plt.savefig('output/1_guessed.png')
 
 # Test 2
 
 index = np.random.randint(59500, 60000+1)
 
-img = images[index]
+img = np.array(images[index].tolist(), order = 'C')
 lbl = labels[index]
 
-output = N.guess(img)
+output = N.guess(np.ascontiguousarray(img[:, np.newaxis]))
 
 plt.imshow(img.reshape(28, 28), cmap="Greys")
 plt.title(f"Is it a {output.argmax()}? It should be a {lbl.argmax()}.")
